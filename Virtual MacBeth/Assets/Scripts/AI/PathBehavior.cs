7faa52d38d3a0fe9f2337e3dd70604cfee667f0b
Tim Seitz <t.seitz972@yahoo.com>
2019-07-01 01:24:09 -0600

    Upload to make the project work with gitIgnore
    By reuploading now, the github shouldn't be cancer anymore because gitignore will work. This is the same project from before this.

---
+++ Virtual MacBeth/Assets/Scripts/AI/PathBehavior.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathBehavior : MonoBehaviour
+{
+    /// <summary>
+    ///  All available paths to the AI
+    /// </summary>
+    public List<Path> paths;
+
+    /// <summary>
+    /// AI's starting path.
+    /// </summary>
+    public int currPath = 0;
+
+    public float turnSpeed = 0.5f;
+
+    public float patrolSpeed = 5f;
+
+    // ===== PRIVATE VARIABLES ==== //
+    /// <summary>
+    /// 
+    /// </summary>
+    private GameObject currNode;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private Quaternion tarQuat;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private bool aimedAtTarget = false;
+
+    private int currPosInPath = 0;
+
+    private void Start()
+    {
+        // Temporary for us
+        Path path = new Path
+        {
+            path = new GameObject[paths[currPath].path.Length]
+        };
+
+        // Get our initial node
+        currNode = paths[currPath].path[0];
+        
+        // Go find the nearest node to go to
+        for(int i = 0; i < paths[currPath].path.Length; i++)
+        {
+            if (Vector3.Distance(paths[currPath].path[i].transform.position, transform.position) <
+                Vector3.Distance(currNode.transform.position, transform.position))
+            {
+                currNode = paths[currPath].path[i];
+                currPosInPath = 0;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(aimedAtTarget == false)
+        {
+            // Look at the target
+            if(transform.rotation.eulerAngles.y < 80f)
+            {
+                RotateToTarget();
+            }
+            else
+            {
+                transform.rotation = tarQuat;
+                aimedAtTarget = true;
+            }
+        }
+        else
+        {
+            // Go ahead and move
+            transform.position = Vector3.MoveTowards(transform.position, currNode.transform.position,
+                patrolSpeed * Time.deltaTime);
+        }
+
+        // Check if our pos == currNode pos
+        if(currNode.transform.position == transform.position)
+        {
+            Debug.Log("here!");
+        }
+
+    }
+
+    private void RotateToTarget()
+    {
+        tarQuat = Quaternion.LookRotation(currNode.transform.position - transform.position);
+        float str = Mathf.Min(turnSpeed * Time.deltaTime, 1f);
+        transform.rotation = Quaternion.Lerp(transform.rotation, tarQuat, str);
+    }
+
+}
---
+++ Virtual MacBeth/Assets/Scripts/Tim/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //What object the camera will be following
+    public Transform target;
+
+    //How much the camera will smooth/lag behind
+    public float smoothSpeed = 0.125f;
+
+    //Camera Offset
+    public Vector3 offset;
+
+    // Same as Update but its just called after the other updates
+    //This was directly from the tutorial I was watching
+    void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
